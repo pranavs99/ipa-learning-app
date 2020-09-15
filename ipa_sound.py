124e6517114c2751796b54ec717d0b9a2930320f
pranavs99 <pranavs99@github.com>
2020-09-14 20:01:02 -0700

    test(IPASound): started first iteration of first test

--- ipa_sound.py
+++ ipa_sound.py
@@ -1,3 +1,5 @@
+import unittest
+
 # the IPASound class is a parent class for Vowel and Consonant because
 # both of these classes share a few fields and methods
 class IPASound:
@@ -15,3 +17,26 @@ class IPASound:
         return self.phonation
     def get_airstream(self):
         return self.airstream
+
+
+
+# testing the IPASound object
+class TestIPASoundClass(unittest.TestCase):
+
+    # testing fields
+    dummy_symbol = "p"
+    dummy_phonation = "unvoiced"
+    dummy_airstream = "pulmonic"
+
+    def test_creation_with_valid_arguments(self):
+        s = IPASound(
+            symbol = self.dummy_symbol,
+            phonation = self.dummy_phonation,
+            airstream = self.dummy_airstream
+        )
+        self.assertEqual(s.get_symbol(), self.dummy_symbol)
+        self.assertEqual(s.get_phonation(), self.dummy_phonation)
+        self.assertEqual(s.get_airstream(), self.dummy_airstream)
+
+    def test_creation_with_invalid_arguments(self):
+        pass
