53595fba2e707a6abfc3c09673a90458859cf76f
pranavs99 <pranavs99@github.com>
2020-09-14 19:34:43 -0700

    refactor(IPASound): created parent class IPASound for child classes Consonant and Vowel

--- consonant.py
+++ consonant.py
@@ -1,23 +1,23 @@
+from ipa_sound import IPASound
+
 # the Consonant class defines an IPA symbol typically distinguished
 # by its place, manner, phonation, and airstream
-class Consonant:
+class Consonant(IPASound):
 
     # constructor
     def __init__(self, symbol, place, manner, phonation, airstream):
-        self.symbol = symbol
+        # inheriting all fields and methods from the parent class IPASound
+        super().__init__(
+            symbol = symbol,
+            phonation = phonation,
+            airstream = airstream
+        )
+        # adding Consonant-specific fields
         self.place = place
         self.manner = manner
-        self.phonation = phonation
-        self.airstream = airstream
 
-    # accessors
-    def get_symbol(self):
-        return self.symbol
+    # Consonant-specific accessors
     def get_place(self):
         return self.place
     def get_manner(self):
         return self.manner
-    def get_phonation(self):
-        return self.phonation
-    def get_airstream(self):
-        return self.airstream
--- vowel.py
+++ vowel.py
@@ -1,27 +1,30 @@
+from ipa_sound import IPASound
+
 # the Vowel class defines an IPA symbol typically distinguished
 # by its height, backness, and rounding
-class Vowel:
+class Vowel(IPASound):
 
     # constructor
     def __init__(self, symbol, height, backness, rounding):
-        self.symbol = symbol
+        # inheriting all fields and methods from the parent class, IPASound
+        # and setting the .phonation & .airstream fields because they're the
+        # same for all consonant
+        super().__init__(
+            symbol = symbol,
+            # all vowels are voiced
+            phonation = "voiced",
+            # all vowels are pulmonic
+            airstream = "pulmonic"
+        )
+        # Vowel-specific fields
         self.height = height
         self.backness = backness
         self.rounding = rounding
-        # all vowels are voiced and pulmonic by default
-        self.phonation = "voiced"
-        self.airstream = "pulmonic"
     
-    # accessors
-    def get_symbol(self):
-        return self.symbol
+    # Vowel-specific accessors
     def get_height(self):
         return self.height
     def get_backness(self):
         return self.backness
     def get_rounding(self):
         return self.rounding
-    def get_phonation(self):
-        return self.phonation
-    def get_airstream(self):
-        return self.airstream
